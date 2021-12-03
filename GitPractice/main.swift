de352feb65826ca1e8c11ac9539abf6fceeb095b
zloysergunya <zloysergunya@mail.ru>
2021-12-03 22:23:05 +0700

    Add fast pow function

--- GitPractice/main.swift
+++ GitPractice/main.swift
@@ -1,12 +1,21 @@
 import Foundation
 
 func my_pow(_ number: Double, degree: UInt) -> Double {
-    var result = number
-    for _ in 1..<degree {
-        result *= number
-    }
+    var number = number
+    var degree = degree
+    
+    var result: Double = 1.0
+    repeat {
+        if degree % 2 == 1 {
+            result *= number
+        }
+        
+        number *= number
+        degree = degree / 2
+        
+    } while degree > 0
     
     return result
 }
 
-print(my_pow(3, degree: 2))
+print(my_pow(3, degree: 3))
