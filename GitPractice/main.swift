247756e290238b7a187df3d3bffb5024f2f373e2
zloysergunya <zloysergunya@mail.ru>
2021-12-03 22:28:43 +0700

    Fix pow function for negative numbers

--- GitPractice/main.swift
+++ GitPractice/main.swift
@@ -1,12 +1,19 @@
 import Foundation
 
-func my_pow(_ number: Double, degree: UInt) -> Double {
-    var result = number
-    for _ in 1..<degree {
-        result *= number
+func my_pow(_ number: Double, degree: Int) -> Double {
+    var result: Double = 1.0
+    
+    if degree > 0 {
+        for _ in 0..<degree {
+            result *= number
+        }
+    } else {
+        for _ in degree..<0 {
+            result /= number
+        }
     }
     
     return result
 }
 
-print(my_pow(3, degree: 2))
+print(my_pow(2, degree: -2))
